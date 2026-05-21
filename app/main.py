873a10f689a32562d97fc1d9e333686d9c8e9328
mpbe <mbell85@ymail.com>
2026-05-21 12:55:20 +0100

    updated for deployment

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routers import users, workouts
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 #create the database tables
 Base.metadata.create_all(bind=engine)
+
+#add CORS middleware to help integration with frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #register the routers when created
 app.include_router(users.router)
@@ -13,4 +23,4 @@ app.include_router(workouts.router)
 
 @app.get("/")
 def homepage():
-    return {"message": "API is running"}
+    return {"message": "head to /docs to preview the app's core functionality"}

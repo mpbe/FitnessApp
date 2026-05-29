d93b20ab11201918b27ed1134cdc46d2188f02c6
mpbe <mbell85@ymail.com>
2026-05-29 14:33:46 +0100

    database migrated to neon postgres for deployment with frontend

--- app/database.py
+++ app/database.py
@@ -4,11 +4,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 DATABASE_URL = config.DATABASE_URL
-engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
